879ec3fda3c0160c7ee1a74c23781ca1bee57443
Luca Paco Palomba <luca.palomba.associati@gmail.com>
2017-12-28 17:41:06 +0100

    Eslint config

--- src/index.js
+++ src/index.js
@@ -8,96 +8,96 @@ import './style.css';
 // TODO convert/test in 16.X ?
 
 class AdaptiveProgressBar extends React.Component {
-  checkPercentagesData(percentagesSeries) {
-    const realPercentages = percentagesSeries.map(
-      ({ value, color }) => {
-        value = value > 100 ? 100 : value < 0 ? 0 : value;
-        color = color || this.props.colors.activeColor;
-        return { value, color };
-      }
-    );
+    checkPercentagesData(percentagesSeries) {
+        const realPercentages = percentagesSeries.map(
+            ({ value, color }) => {
+                value = value > 100 ? 100 : value < 0 ? 0 : value;
+                color = color || this.props.colors.activeColor;
+                return { value, color };
+            }
+        );
 
-    return realPercentages;
-  }
+        return realPercentages;
+    }
 
-  prepareLegendtext(percentagesSeries) {
-    return percentagesSeries.map(percentage => `${percentage.value}%`).join("/");
-  }
+    prepareLegendtext(percentagesSeries) {
+        return percentagesSeries.map(percentage => `${percentage.value}%`).join('/');
+    }
 
-  render() {
-    const {
-      percentages = [],
-      text = false,
-      legend: { enabled: legendEnabled = false },
-      colors: {
-        activeColor = "red",
-        inactiveColor = "blue",
-        labelColor = "white"
-      }
-    } = this.props;
+    render() {
+        const {
+            percentages = [],
+            text = false,
+            legend: { enabled: legendEnabled = false },
+            colors: {
+                activeColor = 'red',
+                inactiveColor = 'blue',
+                labelColor = 'white'
+            }
+        } = this.props;
 
-    // avoid data pollution; TODO, check data all togheter, thresholds and sums
-    const realPercentages = this.checkPercentagesData(percentages);
+        // avoid data pollution; TODO, check data all togheter, thresholds and sums
+        const realPercentages = this.checkPercentagesData(percentages);
 
-    //prepare text;
-    const legendText = legendEnabled
-      ? this.prepareLegendtext(percentages)
-      : null;
+        //prepare text;
+        const legendText = legendEnabled
+            ? this.prepareLegendtext(percentages)
+            : null;
 
-    //change style on results
-    const parentStyle = {
-      backgroundColor: inactiveColor
-    };
+        //change style on results
+        const parentStyle = {
+            backgroundColor: inactiveColor
+        };
 
-    const childStyle = percentage => {
-      return {
-        width: `${percentage.value}%`,
-        backgroundColor: `${percentage.color}`
-      };
-    };
+        const childStyle = percentage => {
+            return {
+                width: `${percentage.value}%`,
+                backgroundColor: `${percentage.color}`
+            };
+        };
 
-    const labelStyle = {
-      color: labelColor
-    };
+        const labelStyle = {
+            color: labelColor
+        };
 
-    // TODO, allow single bar messages instead a Text.
-    return (
-      <div className="progressbar-container">
-        <div className="label" style={labelStyle}>
-          {text}
-        </div>
-        {legendText && (
-          <div className="legend" style={labelStyle}>
-            {legendText}
-          </div>
-        )}
-        <div className="progress" style={parentStyle}>
-          {realPercentages.map(percentage => (
-            <div className="child" style={childStyle(percentage)} />
-          ))}
-        </div>
-      </div>
-    );
-  }
+        // TODO, allow single bar messages instead a Text.
+        return (
+            <div className="progressbar-container">
+                <div className="label" style={labelStyle}>
+                    {text}
+                </div>
+                {legendText && (
+                    <div className="legend" style={labelStyle}>
+                        {legendText}
+                    </div>
+                )}
+                <div className="progress" style={parentStyle}>
+                    {realPercentages.map(percentage => (
+                        <div className="child" style={childStyle(percentage)} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
 }
 
 // TODO propTypes.
 
 render(
-  <AdaptiveProgressBar
-    percentages={[
-      { value: 25.5, color: "#121C25" },
-      { value: 50.2, color: "#13314D" }
-    ]}
-    text={"Time I am writing code in a day"}
-    legend={{
-      enabled: true
-    }}
-    colors={{
-      activeColor: "#121C25",
-      inactiveColor: "#2D587B",
-      labelColor: "#9CB1BF"
-    }}
-  />,
-  document.getElementById("app")
+    <AdaptiveProgressBar
+        percentages={[
+            { value: 25.5, color: '#121C25' },
+            { value: 50.2, color: '#13314D' }
+        ]}
+        text={'Time I am writing code in a day'}
+        legend={{
+            enabled: true
+        }}
+        colors={{
+            activeColor: '#121C25',
+            inactiveColor: '#2D587B',
+            labelColor: '#9CB1BF'
+        }}
+    />,
+    document.getElementById('app')
 );
